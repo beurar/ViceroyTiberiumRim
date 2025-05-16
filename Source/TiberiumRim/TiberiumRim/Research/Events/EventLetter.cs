096bb4d645fdbb779a29ff7570631bc2509cfbbf
Stephanus van Zyl <beurar@gmail.com>
2025-05-16 17:14:33 +0200

    Event Letter error fix (if research was already unlocked)

--- Source/TiberiumRim/TiberiumRim/Research/Events/EventLetter.cs
+++ Source/TiberiumRim/TiberiumRim/Research/Events/EventLetter.cs
@@ -33,32 +33,49 @@ namespace TiberiumRim
             events.Add(eventDef);
         }
 
+        private void CloseLetter()
+        {
+            if (Find.LetterStack.LettersListForReading.Contains(this))
+                Find.LetterStack.RemoveLetter(this);
+            else
+                Log.Warning("[TiberiumRim] Tried to remove EventLetter that was not in stack.");
+        }
+
+
         public override IEnumerable<DiaOption> Choices
         {
             get
             {
                 foreach (var choice in base.Choices)
-                {
                     yield return choice;
-                }
 
-                yield return new DiaOption("TR_OpenTab".Translate())
+                // Check safely before assuming
+                var validEvent = events?.FirstOrDefault(e => !e.unlocksResearch.NullOrEmpty());
+                if (validEvent?.unlocksResearch != null && validEvent.unlocksResearch.Count > 0)
                 {
-                    action = delegate
+                    yield return new DiaOption("TR_OpenTab".Translate())
                     {
-                        Find.MainTabsRoot.SetCurrentTab(TiberiumDefOf.TiberiumTab);
-                        var researchWindow = (MainTabWindow_TibResearch) Find.MainTabsRoot.OpenTab.TabWindow;
-                        researchWindow.SelTab = ResearchTabOption.Projects;
-                        var proj = events.First(e => !e.unlocksResearch.NullOrEmpty()).unlocksResearch.First();
-                        researchWindow.SelProject = proj;
+                        action = delegate
+                        {
+                            Find.MainTabsRoot.SetCurrentTab(TiberiumDefOf.TiberiumTab);
+                            var researchWindow = (MainTabWindow_TibResearch)Find.MainTabsRoot.OpenTab.TabWindow;
+                            researchWindow.SelTab = ResearchTabOption.Projects;
+                            researchWindow.SelProject = validEvent.unlocksResearch.First();
 
-                        var manager = TRUtils.ResearchManager();
-                        if (!manager.IsOpen(proj.ParentGroup))
-                            manager.OpenClose(proj.ParentGroup);
+                            var manager = TRUtils.ResearchManager();
+                            if (!manager.IsOpen(validEvent.unlocksResearch.First().ParentGroup))
+                                manager.OpenClose(validEvent.unlocksResearch.First().ParentGroup);
 
-                        //TODO: Select event
-                    }
-                };
+                            // TODO: Select event
+                        }
+                    };
+                }
+                else
+                {
+                    // Optional fallback so the dialog doesn't look empty
+                    yield return new DiaOption("OK".Translate()) { action = () => CloseLetter() };
+                    Log.Warning("[TiberiumRim] EventLetter: No valid research project found to unlock.");
+                }
             }
         }
     }
